6e6b9a6ab8f8838f22c6ea26e443dbab66a2640a
Bakud Yao <bakudr18@gmail.com>
2022-04-19 00:46:30 +0800

    Init hazard point test

---
+++ Makefile
@@ -0,0 +1,16 @@
+TARGET = hp
+CC ?= gcc
+OBJS := main.o
+
+CFLAGS = -g -Wall -Wextra -Wpedantic -O2 -std=c11 -fsanitize=thread
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CC) $(CFLAGS) $^ -o $@ -lpthread
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $< -o $@
+
+clean:
+	rm -f *.o $(TARGET)
---
+++ main.c
@@ -0,0 +1,140 @@
+#define _POSIX_C_SOURCE 200112L
+#include <assert.h>
+#include <err.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/timerfd.h>
+#include <time.h>
+#include <unistd.h>
+
+#define N_READERS 1
+#define N_WRITERS 7
+#define N_ITERS 20
+#define ARRAY_SIZE(x) sizeof(x) / sizeof(*x)
+
+typedef struct {
+    unsigned int v1;
+} config_t;
+
+typedef uintptr_t hp_t;
+
+static config_t *shared_config;
+static hp_t hp_ptr;
+static pthread_barrier_t barr;
+
+config_t *create_config()
+{
+    config_t *out = calloc(1, sizeof(config_t));
+    if (!out)
+        err(EXIT_FAILURE, "calloc");
+    return out;
+}
+
+void delete_config(void *arg)
+{
+    config_t *conf = (config_t *) arg;
+    assert(conf);
+    free(conf);
+}
+
+static void print_config(const char *name, const config_t *conf)
+{
+    printf("%s : { 0x%08x }\n", name, conf->v1);
+}
+
+void init()
+{
+    shared_config = create_config();
+    hp_ptr = 0;
+}
+
+void deinit()
+{
+    delete_config(shared_config);
+}
+
+/* Only accept one reader */
+static void *reader_thread(void *arg)
+{
+    (void) arg;
+    const uintptr_t nullptr = 0;
+
+    pthread_barrier_wait(&barr);
+
+    for (int i = 0; i < N_ITERS; i++) {
+        while (1) {
+            uintptr_t val =
+                (uintptr_t) __atomic_load_n(&shared_config, __ATOMIC_ACQUIRE);
+
+            __atomic_store(&hp_ptr, &val, __ATOMIC_RELEASE);
+
+            if (__atomic_load_n(&shared_config, __ATOMIC_ACQUIRE) ==
+                (config_t *) val) {
+                print_config("read config  ", (config_t *) val);
+                break;
+            }
+
+            __atomic_store(&hp_ptr, &nullptr, __ATOMIC_RELEASE);
+        }
+    }
+
+    __atomic_store(&hp_ptr, &nullptr, __ATOMIC_RELEASE);
+
+    return NULL;
+}
+
+static void *writer_thread(void *arg)
+{
+    (void) arg;
+
+    pthread_barrier_wait(&barr);
+    // print_config("updating config", new_config);
+
+    for (int i = 0; i < N_ITERS; i++) {
+        config_t *new_config = create_config();
+        new_config->v1 = rand();
+        const uintptr_t old_obj = (uintptr_t) __atomic_exchange_n(
+            &shared_config, new_config, __ATOMIC_ACQ_REL);
+        while (__atomic_load_n(&hp_ptr, __ATOMIC_ACQUIRE) == old_obj)
+            ;
+        delete_config((void *) old_obj);
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t readers[N_READERS], writers[N_WRITERS];
+    pthread_barrier_init(&barr, NULL, N_READERS + N_WRITERS);
+
+    init();
+
+    for (size_t i = 0; i < ARRAY_SIZE(writers); ++i) {
+        if (pthread_create(writers + i, NULL, writer_thread, NULL))
+            warn("pthread_create");
+    }
+
+    for (size_t i = 0; i < ARRAY_SIZE(readers); ++i) {
+        if (pthread_create(readers + i, NULL, reader_thread, NULL))
+            warn("pthread_create");
+    }
+
+    for (size_t i = 0; i < ARRAY_SIZE(writers); ++i) {
+        if (pthread_join(writers[i], NULL))
+            warn("pthread_join");
+    }
+
+    for (size_t i = 0; i < ARRAY_SIZE(readers); ++i) {
+        if (pthread_join(readers[i], NULL))
+            warn("pthread_join");
+    }
+
+    deinit();
+
+    return EXIT_SUCCESS;
+}
